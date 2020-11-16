9c5d939c79c92548e525547fb3e1b754670e1cf6
chuanshaof <63920704+chuanshaof@users.noreply.github.com>
2020-11-16 22:30:36 +0800

    Update BigBrainMonopoly.py
    @DidItWork

--- BigBrainMonopoly.py
+++ BigBrainMonopoly.py
@@ -27,11 +27,15 @@ import matplotlib
 # bankruptcy - how many people are bankrupted
 
 num_of_tiles = 40
+
+# change this into an input later
 num_players = 5
+
+# Do we need to create these empty lists? Purpose?
 tiles = []
 building_pos = []
 building_names = []
-building_colours = []
+building_cost = []
 building_values = []
 chance_pos = []
 jail_pos = []
@@ -47,36 +51,58 @@ bankruptcy = 0
 
 class player():
     
-    #for functions on setting variables and getting variable values, use getters and setters
+    # for functions on setting variables and getting variable values, use getters and setter
+    id_no = 0
     
-    def __init__(self,name):
-        self.name = ""
-        self.status = "normal" #normal, bankrupt, jailed
+    def __init__(self):
+        self.id = id_no
+        self.status = "normal"
+        self.position = 0
+        
+        id_no += 1
+        
     
     def get_status(self):
         return self.status
     
     def update_status(self,status):
+        #normal, bankrupt, jailed
         self.status = status
+    
+    def get_position(self):
+        return self.position
+    
+    def update_position(self, position):
+        self.position = position
+    
         
-
 class building():
     
-    def __init__(self, colour, title, level, values):
+    # Don't need colour cause can't present colours anyways
+    def __init__(self, title, level, cost, values):
         
-        #values is a dictionary with the key being the building name and its value
-        #being a list of three numbers with each number indicating its value at the
-        #respective level
+        # I don't think values should be a dictionary, should just be a list of 3 values and use "level", 0, 1, 2
+        # To get the value. Else values can just be a global dictionary already
         
-        self.colour = colour
+        # values is a dictionary with the key being the building name and its value
+        # being a list of three numbers with each number indicating its value at the
+        # respective level
+        
         self.title = title
         self.level = level
+        self.cost = cost
         self.values = values
-
+        
 class tile():
-    def __init__(self,id_no,tile_type,building):
+    
+    id_no = 0
+    
+    def __init__(self, tile_type, building):
         self.id = id_no
+        id_no += 1
+        
         self.tile_type = tile_type
+        
         if self.tile_type == "building":
             self.building = building
 
@@ -84,21 +110,30 @@ class tile():
 #Initialising variables
 #-------------------------------------------------------------------------#
 
+# Why we have the whole tiles.append() thing for ah lol
+
 for i in range(num_of_tiles):
-    if i==0:
-        tiles.append(tile(i,"home",""))
+    if i == 0:
+        tiles.append(tile("home",""))
     elif i in building_pos:
-        tiles.append(tile(i,"building",building(building_colours[i],building_names[i],0,building_values[i])))
+        tiles.append(tile("building", building(building_names[i], 0, building_cost[i], building_values[i])))
     elif i in chance_pos:
-        tiles.append(tile(i,"chance",""))
+        tiles.append(tile("chance",""))
     elif i in jail_pos:
-        tiles.append(tile(i,"jail",""))
+        tiles.append(tile("jail",""))
     elif i in tax_pos:
-        tiles.append(tile(i,"tax",""))
+        tiles.append(tile("tax",""))
         
 for i in range(num_players):
-    players.append(player(""))
+    
+    # Do you want to add like input for player name?
+    # Is players like a list?
+    
+    players.append(player())
 
+for i in range(num_players):
+    name = input(f"Enter Player{i}'s name")
+    name = player()
 
 #game functions
 #-------------------------------------------------------------------------#
@@ -106,9 +141,11 @@ for i in range(num_players):
 def roll():
     
     #return a tuple of two integers (1-6) from dice rolls
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
     
-    pass
-    
+    return dice1, dice2
+
 def update_board(board):
     
     #return an updated board from the old board
@@ -118,6 +155,7 @@ def update_board(board):
 
 def display_board(board):
     
+    # probably use some print function to do so
     #display board
     
     pass
